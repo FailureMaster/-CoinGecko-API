4450b1538b127a4670d3b47b8f127c1f8f9bd507
FailureMaster <mike.macabulos@gmail.com>
2024-03-13 12:14:09 +0800

    Setup the index.ejs to use the design that I've made in BS Studio

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const bodyParser    = require('body-parser');
 
 const app = express();
 
+app.use(express.static(__dirname + "/assets"));
+// app.use(express.static('./assets'));
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
